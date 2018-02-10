52aea6800bb4ea01b6f09634aef1c108405d1f67
IgnatyiParov <36331809+IgnatyiParov@users.noreply.github.com>
2018-02-10 16:12:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$(document).ready(function(){
-    $(".abs-menu").hide();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    $(".a-menu").click(function(){
-            $(".abs-menu").slideToggle('slow');
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
 
-    });
-
-        var i = 1; 
-
-        /*$("#img").click(function(){*/
-            
-         $("#next").click(function(){ 
-                $("#img").fadeToggle('slow',function(){
-                        i = i+1;
-                        if(i > 4) {
-                                i=1;
-                        }
-                        $("#img").hide().attr('src',"images/im" +i +".jpg");
-                })
-                $("#img").fadeToggle('slow')
-        })
-        $("#last").click(function(){ 
-                $("#img").fadeToggle('slow',function(){
-                        i = i-1;
-                        if(i < 1) {
-                                i=3;
-                        }
-                        $("#img").hide().attr('src',"images/im" +i +".jpg");
-                })
-                $("#img").fadeToggle('slow')
-        }) 
-
-        /*for(var a=0; a<10; a++){
-        setTimeout( func, 2000)
-        }*/
-      /*  });*/
-
-})
 function myMap() {
-        var mapCanvas = document.getElementById("map");
-        var mapOptions = {
-          center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-        };
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-      }
-    
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+$(document).ready(function(){
+  $(".hidemenu ul").hide();
+
+  $(".hidemenu").click(function(){
+          $(".hidemenu ul").slideToggle('slow',function(){
+
+          });
+
+
+
+  });
+})
